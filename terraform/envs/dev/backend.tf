ca32c6b5ba14b0a366542018eca442d5788b850b
gucdeepthi <gucdeepthi9@gmail.com>
2026-05-02 00:35:18 +0530

    Update backend.tf
    bucket updated

--- terraform/envs/dev/backend.tf
+++ terraform/envs/dev/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "dev-terraform-state-yourname"
-    key            = "vpc/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-locks"
+    bucket = "dev-terraform-state-shankarcoding9"
+    key    = "dev/terraform.tfstate"
+    region = "ap-south-1"
   }
 }
+
